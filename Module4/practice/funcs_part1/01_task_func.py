95762a8b3594af04922766d216c94bc691f72b98
Khasan09 <86655038+Khasan09@users.noreply.github.com>
2021-07-01 12:14:12 +0300

    Update 01_task_func.py

--- Module4/practice/funcs_part1/01_task_func.py
+++ Module4/practice/funcs_part1/01_task_func.py
@@ -4,10 +4,10 @@ def max2(n1, n2):
     # TODO: your code here
     pass
 
+def max2(n1, n2):
+    if n1 < n2:
+        return n2
+    return n1
 
-# Тестируем функцию
-print(max2(5, 6))
-print(max2(-10, -12))
-print(max2(2.5, 2.6))
-print(max2(-2.5, 0))
-print(max2(0, -2.5))
+
+print(max2(1, 2))
